d5cf4dd4d8c8cc17805535a54dec0f78f2f1a651
VISHNU SHANKAR SINGH <104004983+VishnuShankarSingh2003@users.noreply.github.com>
2024-08-03 17:58:53 +0530

    Update Preloader.js

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,7 +1,22 @@
-import React from 'react'
+// import React from 'react'
+
+// export default function Preloader(props) {
+//   return (
+//        <div id={props.load ? "preloader" : "preloader-none"}></div>
+//   )
+// }
+
+
+
+import React from 'react';
+import './Preloader.css';
 
 export default function Preloader(props) {
   return (
-       <div id={props.load ? "preloader" : "preloader-none"}></div>
-  )
+    <div className={props.load ? "preloader" : "preloader-none"}>
+      <div className="spinner"></div>
+      <div className="welcome-message">Welcome to Vishnu's Portfolio</div>
+    </div>
+  );
 }
+
